ee12ddd36a1ad20f1ecb74c448a73e1d9e9e91d3
Lachlan Gardner <glachlan365@gmail.com>
2026-05-08 22:04:56 +1200

    Changed code analysis values.

--- Summative-Assessment/obj/Debug/net10.0/Summative-Assessment.AssemblyInfo.cs
+++ Summative-Assessment/obj/Debug/net10.0/Summative-Assessment.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Summative-Assessment")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71c94547ab690dba1e638aee1ba0e53be5f25bd9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52d2d387f782adbab5bb515be61372cc99b9caeb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Summative-Assessment")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Summative-Assessment")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
